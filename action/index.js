1de996d62ac19bedea991eec73bcdd705a5032bd
Christian Glahn <cpglahn@gmail.com>
2024-02-28 21:45:20 +0100

    initial functional version of the action

--- action/index.js
+++ action/index.js
@@ -29080,6 +29080,22 @@ module.exports = require("node:events");
 
 /***/ }),
 
+/***/ 7561:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:fs");
+
+/***/ }),
+
+/***/ 9411:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("node:path");
+
+/***/ }),
+
 /***/ 4492:
 /***/ ((module) => {
 
@@ -30862,14 +30878,144 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
+const fs = (__nccwpck_require__(7561).promises);
+const path = __nccwpck_require__(9411);
 
-try {
-    const message = "well done";
-    
-    console.log(`The event payload: ${message}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+async function run() {
+    try {
+        const message = "well done";
+
+        // check and default inputs
+        // const auto_convert = core.getInput("auto-convert");
+        const close_issue = core.getInput("close-issue") || "";
+        // const gh_token = core.getInput('github-token');
+        const issue_template = core.getInput("issue-template");
+        const label = core.getInput("label");
+        const publish_label = core.getInput("publish-label");
+        const target_folder = core.getInput("target-folder");
+        // const template = core.getInput("template");
+
+        const bool_close = close_issue === "true" || close_issue === "yes" || close_issue === "1";
+
+        const labels = label?.split(",").map(l => l.trim()).filter(l => l.length);
+
+        // load the issue
+        const query_issue = `
+        query( $owner: String!, $repo: String! ${ labels ? ", $labels: [String!]" : "" }) {
+            repository(owner: $owner, name: $repo) {
+              name
+              
+              issues(states: [OPEN], first:100 ${ labels ? ", labels: $labels" : "" }) {
+                    nodes {
+                    author {
+                        login 
+                    }
+                    id
+                    number
+                    title
+                    createdAt
+                    lastEditedAt
+                    body
+                    bodyResourcePath
+                    labels(first:10) {
+                      nodes {
+                        name
+                      }
+                    }
+                  }
+              }
+            }
+        }`;
+        const query_issue_variables = {
+            "owner": github.context.repo.owner,
+            "repo": github.context.repo.repo
+        };
+
+        if (labels) {
+            query_issue_variables.labels = labels;
+        }
+
+        // load the issue template fields
+
+        const issue_result = await github.graphql(query_issue, query_issue_variables);
+
+        const issues = issue_result.repository.issues.nodes;
+
+        if (issues.length > 0) {
+            for (const issue in issues) {
+                // check if the issue is ready for publishing
+                const labeled_publishing = issue.labels?.nodes?.filter(l => l.name === publish_label).length;
+                const user_publishing = false;
+
+                if (!labeled_publishing || !user_publishing) {
+                    core.debug("issue is not ready for publishing");
+                    continue;
+                }
+
+                // const [idate, itime] = issue.lastEditedAt?.split("T") || issue.createdAt.split("T");
+                const title = issue.title;
+
+                if (issue_template) {
+                    // validate the issue template fields
+                }
+
+                // create the issue's target folder
+                await fs.mkdir(path.join(target_folder, `${issue_template}_${issue.id}`), {recursive: true});
+
+                // NOTE: During development attachments remain at github
+
+                // download all attachments and keep the content type
+                // replace all links to point to the correct location of the attachments
+
+                const body = issue.body;
+
+                // ignore issues without body
+                if (!(body && body.length)) {
+                    continue;
+                }
+
+                // create the index.md file with the issue content
+                await fs.writeFile(
+                    path.join(target_folder, `${issue_template}_${issue.id}`, "index.md"),
+                    `# ${title}
+                    
+                    ${body}`
+                );
+
+                if (bool_close) {
+                    const close_issue_query = `mutation($issueId: String!) {
+                        updateIssue(input: {id : $issueId, state: CLOSED, stateReason: COMPLETED}){
+                          issue {
+                            id
+                            number
+                            state
+                          }
+                      }
+                    }`;
+
+                    const ci_variables = {
+                        issueId: issue.id
+                    };
+
+                    // inform GH to close the issue
+                    const result = await github.graphql(close_issue_query, ci_variables);
+
+                    if (result.issue.id === issue.id && result.issue.state === "CLOSED") {
+                        core.info(`Issue ${issue.number} has been closed`);
+                    }
+                }
+            }
+        }
+
+        core.info(`The event payload: ${message}`);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
+
 })();
 
 module.exports = __webpack_exports__;
