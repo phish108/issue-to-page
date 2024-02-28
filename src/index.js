a7281c2b9550ec68f921b50e2f693c814edb590c
Christian Glahn <cpglahn@gmail.com>
2024-02-28 16:27:49 +0100

    add workflow steps

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const github = require('@actions/github');
 
 try {
     const message = "well done";
-    
+
+    // check and default inputs
+    // load the issue template fields 
+    // load the issue 
+    // validate the issue template fields
+    // create the target folder 
+    // download all attachments and keep the content type
+    // replace all links to point to the correct location of the attachments
+    // create the index.md file with the issue content
+
     console.log(`The event payload: ${message}`);
-  } catch (error) {
+} catch (error) {
     core.setFailed(error.message);
-  }
+}
+
